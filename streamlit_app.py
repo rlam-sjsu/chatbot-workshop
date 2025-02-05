2973f5be9761fe805ee4439a34a78a7f3d8926ea
Raymond Lam <140036973+rlam-sjsu@users.noreply.github.com>
2025-02-05 11:24:34 -0800

    change to adobe

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,15 @@ from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 from llama_index.embeddings.gemini import GeminiEmbedding
 
 
-st.set_page_config(page_title="Chat with an expert on the works of Rabindranath Tagore", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
-st.title("Chat with an expert on the works of Rabindranath Tagore")
+st.set_page_config(page_title="Ask me how to use Adobe software.", page_icon="✏️", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.title("Ask me how to use Adobe software.")
 
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
     st.session_state.messages = [
         {
             "role": "assistant",
-            "content": "Ask me a question about Rabindranath Tagore!!",
+            "content": "Ask me a question about how to use Adobe software.",
         }
     ]
 
@@ -28,11 +28,7 @@ def load_data():
     Settings.llm = Gemini(
         model="models/gemini-1.5-flash",
         temperature=1.0,
-        system_prompt="""You are an expert on the work of Rabindrath Tagore.
-        Answer the question using the provided documents, which contain relevant excerpts from the work of Rabindrath Tagore.
-        The context for all questions is the work of Rabindrath Tagore. Whenver possible, include a quotation from the provided excerpts of his work to illustrate your point.
-        Respond using a florid but direct tone, typical of an early modernist writer.
-        Keep your answers under 100 words.""",
+        system_prompt="""You are an expert on Adobe software. You are helping users learn how to use Adobe Creative Cloud software to create designs and more. Provide your answers in concise and effective prose.""",
         api_key = st.secrets.google_gemini_key,
         safe = [
     {
